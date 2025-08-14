7b0033416c6cfeab8027618c119cdbbf1477a834
Passsmpaas <lokeshsingh17628@gmail.com>
2025-08-14 17:03:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -31,7 +31,7 @@ UNSPLASH_ACCESS_KEY = 'RabDRmuXXBobanmwwbvpP5LwoG4J8ox34y5Sstz-9jk'
 # -----------------------------------------------
 UNSPLASH_QUERY = 'animal baby'
 # -----------------------------------------------
-ADMIN_BOT_USERNAME = "@Extractor_txt_bot" #without
+ADMIN_BOT_USERNAME = "@shdkbot" #without
 
 THUMB_URL = os.environ.get("THUMB_URL", "https://i.ibb.co/ZRvCxd5g/STRANGER-BOY.jpg")
 
