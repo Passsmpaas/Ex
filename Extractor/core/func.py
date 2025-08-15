328d2d87f360734642d3d7ab43f94df88a1a3979
Passsmpaas <lokeshsingh17628@gmail.com>
2025-08-15 09:40:21 +0530

    Update func.py

--- Extractor/core/func.py
+++ Extractor/core/func.py
@@ -38,7 +38,7 @@ async def subscribe(app, message):
             try:
                 url = await gen_link(app, update_channel)
                 await message.reply_photo(
-                    photo="https://i.ibb.co/bMK3Sw5t/IMG-20250814-223851.jpg",
+                    photo="https://i.ibb.co/757br8g/IMG-20250815-040316.png",
                     caption=script.FORCE_MSG.format(message.from_user.mention), 
                     reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🤖 ᴊᴏɪɴ ᴜᴘᴅᴀᴛᴇs ᴄʜᴀɴɴᴇʟ 🤖", url=f"{url}")]])
                 )
